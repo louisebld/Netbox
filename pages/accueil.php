6f435f36ab8716f0e4be2b437cc5fe4c048b5937
Louise Bollard <louiisebld@gmail.com>
2021-01-25 22:31:44 +0100

    Create accueil.php

---
+++ pages/accueil.php
@@ -0,0 +1,40 @@
+<div class="container-fluid m-2">
+	<div class="navbar-right">
+		<button type="button" class="btn btn-light">Se connecter</button>
+		<button type="button" class="btn btn-light">S'inscrire</button>
+		<a class="btn btn-success" href="index.php?page=communaute">Communauté</a>
+
+
+	</div>
+</div>
+<div class="container-fluid bruh text-sm-center">
+	<h1 class="display-1 titre mx-auto  w-75">#ShareLife</h1>
+	<h5>Rejoignez vite la communauté</h5>
+	<!-- <button type="button" class="btn btn-light">Découvrir</button> -->
+</div>
+
+<div class="modal-footer">
+
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-sm text-center" >
+			<h5> Créez vos communautés</h5>
+			<div class="col" id="centre">
+				<img class="card-img-top pt-2 img-article-board" src="images/commu.png">
+			</div> 
+		</div>
+		<div class="col-sm text-center">
+			<h5>Partagez vos photos avec le monde entier</h5>
+			<div class="col" id="centre">
+				<img class="card-img-top pt-2 img-article-board" src="images/partage.png">
+			</div> 
+		</div>
+		<div class="col-sm text-center">
+			<h5>Aimez, commentez, partagez</h5>
+			<div class="col" id="centre">
+				<img class="card-img-top pt-2 img-article-board" src="images/like.png">
+			</div> 
+		</div>
+	</div>
+</div>
