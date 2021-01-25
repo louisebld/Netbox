26be1a87e86526b2487fd8fde1113faa1c0ef678
Louise Bollard <louiisebld@gmail.com>
2021-01-25 22:31:50 +0100

    Create entete.php

---
+++ pages/entete.php
@@ -0,0 +1,8 @@
+
+<div class="contener-fluid entete m-2 ">
+	<h5 class="text-sm-center p-2 align-items-center mr-auto ">#ShareLife</h5>
+	<!-- position-absolute -->
+	<a class="btn btn-danger" href="index.php">Accueil</a>
+	<a class="btn btn-success" href="#">Profil</a>
+
+</div>
