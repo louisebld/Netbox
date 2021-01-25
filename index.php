84fecca3e40429da3276099d894dffbea294f9ca
Louise Bollard <louiisebld@gmail.com>
2021-01-25 22:31:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
-<?php 
-include "PHP/PHP.php";
+<?php
 
-if (isset($_SESSION["page"])) {//La Page Courante
-	$page = $_SESSION["page"];
-} else {
-	$page = "accueil";
-}
+// Index : session start et include
 
-if (isset($_SESSION["id_Profil"])) {//L'id du Profil
-	$id_Profil = $_SESSION["id_Profil"];
-} else {
-	$id_Profil = -1;
-}
-
-if ($page == "accueil") {
-	include "View/accueil.php";
-} else {
-	echo "Erreur de chargement !";
-}
+session_start();
 
 
+// include 'PHP/functions/db.php';
+include 'view.php';
+include 'PHP/action.php';
 
- ?>
+?>
