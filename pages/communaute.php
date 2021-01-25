0b90b0cd405f408c5e8ff471ed833cb78ecb7f88
Louise Bollard <louiisebld@gmail.com>
2021-01-25 22:31:47 +0100

    Create communaute.php

---
+++ pages/communaute.php
@@ -0,0 +1,27 @@
+<div class="contener gros p-4 m-4 text-sm-center">
+<h4>En cours de construction, ne modifiez rien sur cette page svp (sinon conflit)</h4>
+</div>
+
+
+
+<div class="contener m-5 communaute p-4">
+
+<h6>Créez votre communauté !</h6>
+<div class="formulairegroupe m-auto">
+<form>
+  <div class="form-group">
+    <!-- <label for="nom">Nom de votre communauté</label> -->
+    <input type="text" class="form-control" id="nom" placeholder="Nom de votre communauté">
+  </div>
+  <!-- <button type="submit" class="btn btn-primary">Créer</button> -->
+</form>
+
+</div>
+
+</div>
+
+<div class="contener m-5 communaute p-4">
+
+Découvrez les communautés déjà existantes..
+</div>
+
