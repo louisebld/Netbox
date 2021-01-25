868603ec5bf4a94bc0eaa72cb902fb02efe069b7
JonathanDum <77625566+JonathanDum@users.noreply.github.com>
2021-01-25 15:59:25 +0100

    V1

---
+++ View/accueil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Project</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="CSS/Style.css">
+</head>
+<body>
+	<h1>Project</h1>
+</body>
+</html>
--- test.php
+++
@@ -1,2 +0,0 @@
-coucou
-mais c'est modifié
