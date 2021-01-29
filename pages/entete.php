711947df13b001e6ecfcfbaaee68b0ac1ef86afd
Louise <72506187+louisebld@users.noreply.github.com>
2021-01-29 14:57:14 +0100

    Update entete.php

--- pages/entete.php
+++ pages/entete.php
@@ -1,8 +1,36 @@
 
-<div class="contener-fluid entete m-2 ">
+<!-- <div class="contener-fluid entete m-2 ">
 	<h5 class="text-sm-center p-2 align-items-center mr-auto ">#ShareLife</h5>
-	<!-- position-absolute -->
+	position-absolute
 	<a class="btn btn-danger" href="index.php">Accueil</a>
 	<a class="btn btn-success" href="#">Profil</a>
+	<a class="btn btn-success" href="index.php?page=communaute">Communauté</a>
 
-</div>
+</div> -->
+
+
+<nav class="navbar navbar-expand-md navbar-light bg-warning sticky-top">
+       <a href="index.php?page=accueil" class="navbar-brand">
+       	#ShareLife
+           <img src="images/community.png" width="50">
+       </a>
+       <button class="navbar-toggler" data-toggle="collapse" data-target="#menuhamburger">
+           <span class="navbar-toggler-icon"></span>
+          </button>
+       <div class="collapse navbar-collapse" id="menuhamburger">
+         <ul class="navbar-nav m-auto">
+           <!-- <li class="nav-item active">
+             <a href="#" class="nav-link">Accueil</a>
+           </li> -->
+           
+         </ul>
+<!-- 
+                  <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search">
+        <button class="btn btn-primary invisible" type="submit">Envoyer</button>
+          </form>
+ -->
+           <img src="images/user.png" width="50">
+
+       </div>
+</nav>
