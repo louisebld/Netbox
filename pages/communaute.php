d80bb07c8be00eeee53e10639bc36eb3085c9685
Louise <72506187+louisebld@users.noreply.github.com>
2021-01-29 14:57:11 +0100

    Update communaute.php

--- pages/communaute.php
+++ pages/communaute.php
@@ -1,25 +1,49 @@
-<div class="contener gros p-4 m-4 text-sm-center">
+<!-- <div class="contener gros p-4 m-4 text-sm-center">
 	<h4>En cours de construction, ne modifiez rien sur cette page svp (sinon conflit)</h4>
 </div>
-
+ -->
 
 
 <div class="contener m-5 communaute p-4">
 
-	<h6>Créez votre communauté !</h6>
+	<h4>Créez votre communauté !</h4>
 	<div class="formulairegroupe m-auto">
 
 		<?php
 
 		print_formulairecreationcommu();
-		?>
+		if (isset($_SESSION['erreurcreatcommu'])) {
+			echo "<ul>";
+	// on affiche chaque erreur
+			foreach($_SESSION["erreurcreatcommu"] as $faute)
+				echo "<li>$faute</li>";
+			echo "</ul>";
 
+		}
+		?>
 	</div>
 
 </div>
 
 <div class="contener m-5 communaute p-4">
 
-	Découvrez les communautés déjà existantes..
+	<h4>Découvrez les communautés déjà existantes..</h4>
+
+	<div class='container mt-4'>
+        <form method='post' class='form-group' action='index.php?page=communaute'>
+          <input class='form-control mr-sm-2' name='recherchecommu' type='text' placeholder='Communauté' aria-label='Search'>
+
+          <input type='submit' name='cherchercommu' class='btn btn-outline-primary' value='Chercher'/>
+        </form>
+   
+    </div>
+
+
+<?php
+affichecommun($tableaucommu);
+
+?>
+
+
 </div>
 
