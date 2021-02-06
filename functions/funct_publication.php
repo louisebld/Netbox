6e3addb369535c4108b25b4fd038334ea98370d1
Louise <72506187+louisebld@users.noreply.github.com>
2021-02-06 15:07:12 +0100

    ajout fction liées aux publications

--- functions/funct_publication.php
+++ functions/funct_publication.php
@@ -9,12 +9,12 @@ function print_formulairecreationpost() {
 
 
 		<div class="form-group">
-			<input type="file" name="imagecom" class="form-control">
+			<input type="file" name="imagepost" class="form-control">
 		</div>
 
 		<br/>
 		<div class="form-group">
-			<textarea class="form-control" name="description" id="description" placeholder="Légende de votre publication" rows="3"><?php if (isset($_SESSION['donnecreatcommu']['description'])) echo $_SESSION['donnecreatcommu']['description']; ?></textarea>
+			<textarea class="form-control" name="description" id="description" placeholder="Légende de votre publication" rows="3"><?php if (isset($_SESSION['donnecreatpost']['description'])) echo $_SESSION['donnecreatpost']['description']; ?></textarea>
 		</div>
 
 		<input type="hidden" id="idutilisateur" name="idutilisateur" value="<?php if (isset($_SESSION['id'])) {echo $_SESSION['id'];} ?>" />
@@ -26,10 +26,92 @@ function print_formulairecreationpost() {
 
 
 		<br/>
-		<center><input type="submit" class="btn btn-primary" name="creercommu" id="creercommu" value="Créer" /></center>
+		<center><input type="submit" class="btn btn-primary" name="poster" id="poster" value="Créer" /></center>
 
 	</form>
 
 	<?php
+
+}
+
+
+function affiche_imagepost($nomimage){
+
+	return $img = '<img src="./images/post/' . $nomimage . '" alt="post" class="card-img-top pt-2 img-article-board"/>';
+
+}
+
+
+
+
+function affichepost($tableaupost){
+
+	// BUT : afficher les communauté
+
+	// $tableaucommu : tableau associatif contenant les infos des communautés
+
+	echo"<div class='container images-wrapper d-flex'>";
+		echo "<div class='row'>";
+
+	foreach ($tableaupost as $key => $value) {
+		//Affichage
+		echo "<div class='col-lg-4 col-md-12 mb-4 text-center width-auto'>";
+			echo '<div class="thumbnail">';
+			echo "<a class='stylelien' href=index.php?page=post" . $value['idpost'] . ">";
+					echo affiche_imagepost($value['image']);
+					echo '<div class="caption img-thumbnail">';
+						echo $value["description"];
+					echo "</div>";
+			echo "</div>";
+
+		echo "</div>";
+
+	}
+		echo "</div>";
+	echo "</div>";
+}
+
+
+function commenceparpost($chaine) {
+
+  if(strpos($chaine, "post" ) === 0){
+      return true;
+  }else {
+      return false;
+}
+
+}
+
+function savoirpost($chaine){
+	return substr($chaine, 4, (strlen($chaine)-1));
+
+}
+
+
+function recupdonnepost($idpost){
+
+	global $db;
+	$sql = "SELECT * FROM publication WHERE idpost = $idpost";
+	$result=  mysqli_query($db, $sql);
+
+	//on met dans un tableau
+	$tableau = [];
+	while ($row=mysqli_fetch_assoc($result)) {
+		$tableau[] = $row;
+	}
+
+	return $tableau;
+
+
+}
+
+
+function affichemonpost($donnepost){
+
+	echo affiche_imagepost($donnepost[0]['image']);
+	echo $donnepost[0]['description'];
+
+
+
 
 }
