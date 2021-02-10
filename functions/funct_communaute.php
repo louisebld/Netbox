e6f18e2c863c5edf995c06aa9523018f21057856
Louise <72506187+louisebld@users.noreply.github.com>
2021-02-10 18:24:04 +0100

    ajout fonction suppression photo commu

--- functions/funct_communaute.php
+++ functions/funct_communaute.php
@@ -15,8 +15,8 @@ function get_file_extension($file) {
 
 		// $file : contient le nom d'un fichier
 
-	    return substr(strrchr($file,'.'),1);
-	}
+	return substr(strrchr($file,'.'),1);
+}
 
 function enleveespacemaj($chaine) {
 	$ch=str_replace(' ','',$chaine);
@@ -75,25 +75,25 @@ function affichecommun($tableaucommu){
 	// $tableaucommu : tableau associatif contenant les infos des communautés
 
 	echo"<div class='container images-wrapper d-flex'>";
-		echo "<div class='row'>";
+	echo "<div class='row'>";
 
 	foreach ($tableaucommu as $key => $value) {
 		//Affichage
 		echo "<div class='col-lg-4 col-md-12 mb-4 text-center width-auto'>";
-			echo '<div class="thumbnail">';
-			echo "<a class='stylelien' href=index.php?page=commu" . $value['nom'] . ">";
+		echo '<div class="thumbnail">';
+		echo "<a class='stylelien' href=index.php?page=commu" . $value['nom'] . ">";
 					// echo	"<img class='card-img-top pt-2 img-article-board' src='images/commu.png'>";
-					echo affiche_imagecommu($value['image']);
-					echo '<div class="caption img-thumbnail">';
-						echo "<h5>" . $value["nom"] . "</h5>\n";
-						echo $value["description"];
-					echo "</div>";
-			echo "</div>";
+		echo affiche_imagecommu($value['image']);
+		echo '<div class="caption img-thumbnail">';
+		echo "<h5>" . $value["nom"] . "</h5>\n";
+		echo $value["description"];
+		echo "</div>";
+		echo "</div>";
 
 		echo "</div>";
 
 	}
-		echo "</div>";
+	echo "</div>";
 	echo "</div>";
 }
 
@@ -104,15 +104,19 @@ function affichecommun($tableaucommu){
 
 function commenceparcommu($chaine) {
 
-  if(strpos( $chaine, "commu" ) === 0){
-      return true;
-  }else {
-      return false;
-}
+	if(strpos( $chaine, "commu" ) === 0){
+		return true;
+	}else {
+		return false;
+	}
 
 }
 
 function savoircommu($chaine){
 	return substr($chaine, 5, (strlen($chaine)-1));
 
+}
+
+function supprimephotocommu($nomphoto){
+	unlink('images/commu/' . $nomphoto);
 }
