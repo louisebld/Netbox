e3c40ae80bd3e494094845dfe35c9e891c23663a
Cegeeenre <73913043+Cegeeenre@users.noreply.github.com>
2021-02-16 18:13:32 +0100

    Create barre.php

---
+++ pages/barre.php
@@ -0,0 +1,8 @@
+	<div class="fixed-bottom text-end">
+		<a href='index.php?page=publication'><button type="submit" name="addpost" value="post" class="btn btn-primary btn-circle btn-xl bi-pencil-square m-1 "></button></a>
+		<a href='#'><button type="submit" name="like" value="like" class="btn btn-danger  btn-circle btn-xl bi-heart-half m-1"></button></a>
+		<a href='index.php?page=communaute'><button type="submit" name="commu" value="commu" class="btn btn-warning  btn-circle btn-xl bi-journal-richtext m-1"></button></a>
+	</div>
+
+
+	
