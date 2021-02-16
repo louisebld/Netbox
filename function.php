9d6d4ad7a36ed5a7ab98e59966a2c4dd49da4d92
Cegeeenre <73913043+Cegeeenre@users.noreply.github.com>
2021-02-16 18:12:42 +0100

    Update function.php

--- function.php
+++ function.php
@@ -3,10 +3,11 @@
 include 'functions/funct_communaute.php';
 include 'functions/funct_connection.php';
 include 'functions/funct_publication.php';
-
+include 'functions/funct_com.php';
 
 include 'functions/funct_bddcommunaute.php';
 include 'functions/funct_bddinscription.php';
 include 'functions/funct_bddpublication.php';
+include 'functions/funct_bddcom.php';
 
  ?>
