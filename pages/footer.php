2b26edcb562da8e582c467cb963d909b8c150d1e
Cegeeenre <tomthierry73@gmail.com>
2021-02-20 12:52:51 +0100

    Update footer.php

---
+++ pages/footer.php
@@ -0,0 +1,24 @@
+<footer class="barre">
+		<div class="container-fluid mt-5" style="height: 300px">
+			<div class="row text-light text-center">
+				<div class="col-lg-4 mt-5">
+					<h5>A propos</h5>
+					<p> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+				</div>
+				<div class="col-lg-4 mt-5">
+					<h5>Liens utiles</h5>
+					<ul class="list-unstyled">
+						<li><a class="mx-4 LinkFooter hLinkFooter" style="text-decoration: none;" href="index.php?page=accueil">Accueil</a></li>
+						<li><a class="mx-4 LinkFooter hLinkFooter" style="text-decoration: none;" href="index.php?page=communaute">Espace communautaire</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-4 mt-5">
+					<h5>nous contacter</h5>
+					<ul  class="list-unstyled">
+						<!-- <li>Formulaire de contact</li>
+						<li>Par mail à netbox@netbox.com</li> -->
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
