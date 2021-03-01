b36fff2dea137d8c298e96516ae65f47fc75d080
louisebld <louiisebld@gmail.com>
2021-03-01 14:56:44 +0100

    include

--- function.php
+++ function.php
@@ -7,6 +7,8 @@ include 'functions/funct_connection.php';
 include 'functions/funct_profil.php';
 include 'functions/funct_publication.php';
 include 'functions/funct_profilpersonne.php';
+include 'functions/funct_admincommu.php';
+
 
 include 'functions/funct_bddlike.php';
 include 'functions/funct_bddcommunaute.php';
@@ -14,4 +16,6 @@ include 'functions/funct_bddinscription.php';
 include 'functions/funct_bddpublication.php';
 include 'functions/funct_bddcom.php';
 include 'functions/funct_bddrejointcommu.php';
+include 'functions/funct_bddadmincommu.php';
+
  ?>
