be3915f1ec0b1767fafbf1a993dc3fc4f144feb7
louisebld <louiisebld@gmail.com>
2021-03-06 08:29:31 +0100

    include

--- action.php
+++ action.php
@@ -9,5 +9,6 @@ include 'actions/action_publication.php';
 include 'actions/action_com.php';
 include 'actions/action_joincommu.php';
 include 'actions/action_downloadpost.php';
+include 'actions/action_admincommu.php';
 
  ?>
