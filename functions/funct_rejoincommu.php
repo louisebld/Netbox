45024db4efc32b6980e7525a42a435c28f6bb6ea
louisebld <louiisebld@gmail.com>
2021-03-06 08:30:45 +0100

    deplacement

--- functions/funct_rejoincommu.php
+++ functions/funct_rejoincommu.php
@@ -53,29 +53,3 @@ function formulairerejointcommu($idcommunaute) {
 }
 
 
-function affichemembre ($membrecommu, $namedatabase) {
-
-	foreach ($membrecommu as $key => $value) {
-		//Affichage des commentaires
-			?>
-			<img class="roundedImage" src="DATA/profil_pp/<?php echo $value['picture']; ?>" >
-<?php
-			echo "<a class ='stylelien' href=index.php?page=personneid" . $value[$namedatabase] . ">" . $value["pseudo"] . '</a>';
-
-			// echo "<a class='stylelien' href=index.php?page=commu" . $value['nom'] . ">";
-}
-}
-
-function affichephotomembre($membre){
-	foreach ($membre as $key => $value) {
-		?>
-		<img class="roundedImage" src="DATA/profil_pp/<?php echo $value['picture']; ?>" >
-		<?php
-	}
-}
-
-function affichepseudomembre($membre, $namedatabase){
-	foreach ($membre as $key => $value) {
-		echo "<a class ='stylelien' href=index.php?page=personneid" . $value[$namedatabase] . ">" . $value["pseudo"] . '</a>';
-	}
-}
