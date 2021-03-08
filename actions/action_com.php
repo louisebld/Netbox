814c308a7b959eecee24c000f62859687cac618a
Cegeeenre <tomthierry73@gmail.com>
2021-03-08 15:48:19 +0100

    modif com (com vide)

--- actions/action_com.php
+++ actions/action_com.php
@@ -4,22 +4,24 @@
     if (isset($_POST['envoyer_com'])){
 
 
-            $autor = $_SESSION['id'];
+        $autor = $_SESSION['id'];
 
-            $idcomu = $_POST['idcomu'];
-            $idpost = $_POST['idpost'];
-            $com = remplaceApo($_POST["com"]);
-            //recuperation de l'id du projet, pour savoir a quel projet correspond le commentaire
+        $idcomu = $_POST['idcomu'];
+        $idpost = $_POST['idpost'];
+        $com = remplaceApo($_POST["com"]);
+        //recuperation de l'id du projet, pour savoir a quel projet correspond le commentaire
 
-            date_default_timezone_set('Europe/Paris');
-            $date = date('d-m-y h:i:s');
-
+        date_default_timezone_set('Europe/Paris');
+        $date = date('d-m-y h:i:s');
+        if (trim($com)){
             insert_com($autor, $com, $idcomu, $idpost, $date);
-            // Insetion du commentaire dans la table avis
-
-            // on informe l'utilisateur qu'il a posté un commentaire
-            // redirection vers la page projets
-            $post = "post" . $idpost; 
-            header("Location:index.php?page=$post");
+            
         }
+        // Insetion du commentaire dans la table avis
+
+        // on informe l'utilisateur qu'il a posté un commentaire
+        // redirection vers la page projets
+        $post = "post" . $idpost; 
+        header("Location:index.php?page=$post");
+    }
 
