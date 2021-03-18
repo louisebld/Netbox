f76e77503d5638c304057c44ae58cb39a0bdc4f5
louisebld <louiisebld@gmail.com>
2021-03-18 18:34:44 +0100

    affiche commu pas rejoint

--- functions/funct_communaute.php
+++ functions/funct_communaute.php
@@ -149,7 +149,7 @@ function affichecommun($tableaucommu){
 				if (!estbannicommu($_SESSION['id'], $value['idcommu'])) {
 
 					if (isset($_SESSION['id'])) {
-						if (!estdanscommu($_SESSION['id'], $value['idcommu'])) {
+						if (!estdanscommu($_SESSION['id'], $value['idcommu']) && $value['idcreateur'] != $_SESSION['id']) {
 						echo '<h5 class="card-header">';
 						$communaute= $value['idcommu'];
 						formulairerejointcommu($communaute);
@@ -205,4 +205,57 @@ function new_commu (){
 	echo '<div class="d-flex justify-content-end">';
 	echo '<button type="button" class="button_fplan btn btn-danger btn-lg" disabled>New</button>';
 	echo '</div>';
+}
+
+
+function affichecommunonly($tableaucommu){
+
+	// BUT : afficher les communauté
+
+	// $tableaucommu : tableau associatif contenant les infos des communautés
+
+	echo"<div class='container images-wrapper d-flex'>";
+		echo "<div class='card-columns'>";
+
+		foreach ($tableaucommu as $key => $value) {
+			//Affichage
+			// var_dump($value['idcreateur']);
+			if (!estdanscommu($_SESSION['id'], $value['idcommu']) && $_SESSION['id'] != intval($value['idcreateur'])) {
+				echo '<div class="card" style="width: 18rem;">';
+
+
+
+				if (!estbannicommu($_SESSION['id'], $value['idcommu'])) {
+
+					if (isset($_SESSION['id'])) {
+						if (!estdanscommu($_SESSION['id'], $value['idcommu'])) {
+						echo '<h5 class="card-header">';
+						$communaute= $value['idcommu'];
+						formulairerejointcommu($communaute);
+						echo '</h5>';
+						}
+					}
+
+					if (estdanscommu($_SESSION['id'], $value['idcommu'])) {
+						// affichage du bouton de nombres de notif
+						// soustrait le nombre de post dans la commu - le nombre de post vu
+						// l'afficher que quand y'a des notifs ?
+						echo '<button class="btn btn-warning boutonnbpost disabled btn-circle btn-lg">'. (combienpostcommu(recuppost($value['idcommu'])) - postvucommu($_SESSION['id'], $value['idcommu'])) . '</button>';
+					}
+				
+					echo '<h5 class="card-title">';
+					echo "<a class='stylelien' href=index.php?page=commu" . $value['nom'] . ">";
+					echo '</h5>';
+					echo '<div class="image_button_superposed">';
+						echo affiche_imagecommu($value['image']);
+					echo '</div>';
+					echo '<div class="card-body">';
+						echo '<p class="card-text"><h5>' . $value["nom"] . '</h5>' . $value["description"] . '</p>';
+					echo '</div>';
+				echo '</div>';
+			}
+		}
+	}
+		echo "</div>";
+	echo "</div>";
 }
