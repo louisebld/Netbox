766ef4885a96089ea262b9cc873f27dbbb54eba3
louisebld <louiisebld@gmail.com>
2021-03-29 17:18:55 +0200

    formulaire follow unfollow

---
+++ actions/action_follow.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_POST['follow'])) {
+
+
+	$idfollow = $_POST['idutilisateurcourant'];
+	$idfollower = $_SESSION['id'];
+
+	insert_follow($idfollow, $idfollower);
+
+	header("Location:index.php?page=personneid" . $idfollow);
+
+	}
+
+
+if (isset($_POST['unfollow'])) {
+
+
+	$idfollow = $_POST['idutilisateurcourant'];
+	$idfollower = $_SESSION['id'];
+
+	delete_follow($idfollow, $idfollower);
+
+	header("Location:index.php?page=personneid" . $idfollow);
+
+	}
