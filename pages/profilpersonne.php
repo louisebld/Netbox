1d585dc0be1f573712b66100b91d2a081a502ac0
louisebld <louiisebld@gmail.com>
2021-03-29 17:19:47 +0200

    boutons follow unfollow sur le profil

--- pages/profilpersonne.php
+++ pages/profilpersonne.php
@@ -5,8 +5,22 @@ $profil = recup_profil_id($idpersonne)[0];
 
 
 <div style="margin-left: 12.5%; width: 75%; margin-top: 50px;">
+								<?php
+
+	if ($_SESSION['id']!= $idpersonne){
+		if (follow($_SESSION['id'], $idpersonne)){
+			afficheboutonunfollow($idpersonne);
+
+		}else {
+			afficheboutonfollow($idpersonne);
+
+		}
+}
+		?>
 		<img style="width: 50%; margin-left: 25%; margin-top: 50px;" src="DATA/profil_pp/<?php echo $profil['picture']; ?>" >
+
 		<div style="display: flex;">
+
 			<div style="width: 49%; margin: 0;">Pseudo : <?php echo $profil['pseudo']; ?></div>
 		</div>
 		<hr>
@@ -14,6 +28,7 @@ $profil = recup_profil_id($idpersonne)[0];
 			<p>Description :</p>
 			<p style="width: 80%;margin-left: 10%; border:solid black 1px; padding: 5px;"><?php echo $profil['description']; ?></p>
 		</div>
+
 
 		<div>
 			<p> Ses communautés :</p>
