324cbf1778832c6e35aa6d33383526efe297b703
louisebld <louiisebld@gmail.com>
2021-05-07 21:42:18 +0200

    ajout rond recherche

--- pages/barre.php
+++ pages/barre.php
@@ -2,6 +2,7 @@
 		<a href='index.php?page=publication'><button type="submit" name="addpost" value="post" class="btn btn-primary btn-circle btn-xl bi-pencil-square m-1 "></button></a>
 		<a href='index.php?page=likes'><button type="submit" name="like" value="like" class="btn btn-danger  btn-circle btn-xl bi-heart-half m-1"></button></a>
 		<a href='index.php?page=communaute'><button type="submit" name="commu" value="commu" class="btn btn-warning  btn-circle btn-xl bi-journal-richtext m-1"></button></a>
+		<a href='index.php?page=recherche'><button type="submit" name="commu" value="commu" class="btn btn-success  btn-circle btn-xl bi-search m-1"></button></a>
 	</div>
 
 
