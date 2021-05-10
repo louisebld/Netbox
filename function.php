fa761af55a6521830d60690f56448f42502497d5
Cegeeenre <73913043+Cegeeenre@users.noreply.github.com>
2021-05-10 14:41:46 +0200

    Update function.php

--- function.php
+++ function.php
@@ -10,6 +10,7 @@ include 'functions/funct_com.php';
 include 'functions/funct_rejoincommu.php';
 include 'functions/funct_communaute.php';
 
+include 'functions/function_accueil.php';
 include 'functions/funct_connection.php';
 include 'functions/funct_profil.php';
 include 'functions/funct_publication.php';
